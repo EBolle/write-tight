583ae98d90ca20859836c348f0c732eafe35408c
EBolle <ernstbolle@gmail.com>
2022-07-31 12:10:47 +0200

    Added the pattern tests from the write-tight CLI package

---
+++ tests/test_non_default_patterns.py
@@ -0,0 +1,20 @@
+"""Testing my understanding of the default patterns."""
+import re
+
+import pytest
+
+from write_tight.src.non_default_patterns import passive_voice
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("Am I?", [("Am", "I")]),
+        ("Aren't you the best?", []),
+        ("We have been walking for hours now.", [("been", "walking")]),
+        ("So how have you been? Great.", []),
+        ("It was             amazing", [("was", "amazing")]),
+    ],
+)
+def test_passive_voice(test_input: str, expected: str):
+    assert re.findall(passive_voice.pattern, test_input) == expected
