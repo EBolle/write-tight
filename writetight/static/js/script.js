38abe013a7e98207759d068216744dbbb068d109
EBolle <ernstbolle@gmail.com>
2022-08-01 09:16:03 +0200

    Added the start of a JS function to 'listen' to a change in return text, which in turns removes the form to create more room, also add slow transitions etc to make it nice and add a refresh button

--- writetight/static/js/script.js
+++ writetight/static/js/script.js
@@ -1,7 +1,14 @@
 "use strict";
 
-const returnText = document.querySelector(".return-text");
+// HTML elements
+
+const form = document.querySelector("form");
 const patternExps = document.querySelector(".pattern-explanations");
+const returnText = document.querySelector(".return-text");
+const submitButton = document.querySelector(".submit-button");
+
+// Variables
+
 const patterns = [
   "ambiguous-openings",
   "ambiguous-pronouns",
@@ -9,6 +16,21 @@ const patterns = [
   "subjunctive-mood",
   "words-ending-with-ly",
 ];
+
+// Hide the form when returnText is visible [this finally works!, now make it neat and add a REFRESH button]
+
+const hideForm = function () {
+  if (returnText.textContent) {
+    form.classList.add("hide");
+    returnText.style.marginTop = "10rem";
+  } else {
+    setTimeout(hideForm, 300);
+  }
+};
+
+hideForm();
+
+// Reveal pattern explanation on mouseover
 
 patternExps.querySelectorAll("p").forEach(function (element) {
   element.classList.add("hide");
