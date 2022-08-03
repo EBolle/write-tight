1b26c27513c0349caba010d701e61fe9e7785574
EBolle <ernstbolle@gmail.com>
2022-08-03 14:53:01 +0200

    Significantly reduced the complexity of the project by relying on core html functionality instead of JavaScript. Still leveraged JavaScript to show only the matched patterns

--- writetight/static/js/script.js
+++ writetight/static/js/script.js
@@ -2,66 +2,30 @@
 
 // HTML elements
 
-const form = document.querySelector("form");
-const patternExps = document.querySelector(".pattern-explanations");
-const refreshButton = document.querySelector(".refresh-button");
-const returnText = document.querySelector(".return-text");
-const submitButton = document.querySelector(".submit-button");
 const noPatterns = document.querySelector(".no-patterns");
+const patternExpParagraphs = document
+  .querySelector(".pattern-explanations")
+  .querySelectorAll("p");
+const patternSet = new Set();
+const returnTextSpans = document
+  .querySelector(".text-output")
+  .querySelectorAll("span");
 
-// Variables
+// Reveal pattern explanations when a pattern is matched, start by hiding all explanations
 
-const patterns = [
-  "ambiguous-openings",
-  "ambiguous-pronouns",
-  "passive-voice",
-  "subjunctive-mood",
-  "words-ending-with-ly",
-];
-
-// There are only two functionalities for now: unhide patterns that are matched and unhide no patterns matched
-// The refresh button should simply link to the input() function
-
-// Hide the form when returnText is visible [this finally works!, now make it neat and add a REFRESH button]
-
-const hideForm = function () {
-  noPatterns.classList.add("hide");
-  if (returnText.textContent) {
-    form.classList.add("hide");
-    if (returnText.querySelectorAll("span").length === 0) {
-      noPatterns.classList.remove("hide");
-    }
-  } else {
-    setTimeout(hideForm, 300);
-  }
-};
-
-hideForm();
-
-// Reveal pattern explanation on mouseover
-
-const hideExplanations = function () {
-  patternExps.querySelectorAll("p").forEach(function (element) {
-    element.classList.add("hide");
-  });
-};
-
-hideExplanations();
-
-returnText.addEventListener("mouseover", function (event) {
-  const classValue = event.target.classList.value;
-
-  if (patterns.includes(classValue)) {
-    patternExps.querySelector(`.${classValue}-exp`).classList.remove("hide");
-  }
+patternExpParagraphs.forEach(function (element) {
+  element.classList.add("hide");
 });
 
-// Reset on click refresh-button
+returnTextSpans.forEach(function (element) {
+  patternSet.add(`${element.className}-exp`);
+});
 
-refreshButton.addEventListener("click", function (event) {
-  form.classList.remove("hide");
-  returnText.textContent = "";
-  noPatterns.classList.add("hide");
+patternExpParagraphs.forEach(function (element) {
+  const classValue = element.classList[0];
 
-  hideExplanations();
+  if (patternSet.has(classValue)) {
+    element.classList.remove("hide");
+    noPatterns.classList.add("hide");
+  }
 });
