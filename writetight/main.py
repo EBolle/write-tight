9f25ada8725aef59d990d9c9d804559e019bbc5c
EBolle <ernstbolle@gmail.com>
2023-12-30 20:51:24 +0100

    Implemented the new and faster way or going through the text file, leveraging the ascending order of matches

--- writetight/main.py
+++ writetight/main.py
@@ -22,6 +22,28 @@ matcher.add("ly_adverbs", [ly_adverbs])
 def main():
     text = get_text_file()
     text_lines = text.splitlines()
+    text_lines_num = [(line_num, line) for line_num, line in enumerate(text_lines)]
 
-    for line_num, line in enumerate(text_lines, start=1):
-        print(line_num, line)
+    doc = nlp(text)
+    text_tokens = [token.text for token in doc]
+    matches = matcher(doc)
+
+    # matches is a list with matches in ascending order
+    current_match = matches.pop(0)
+    current_line_num = 0
+
+    while matches:
+        pattern_id = current_match[0]
+        pattern = nlp.vocab.strings[pattern_id]
+
+        match_start_token = current_match[1]
+        match_end_token = current_match[2]        
+        match = " ".join(text_tokens[match_start_token:match_end_token])
+
+        current_line = text_lines_num[current_line_num][1]
+
+        if match in current_line:
+            print(current_line_num + 1, pattern, match, current_line)
+            current_match = matches.pop(0)
+        else:
+            current_line_num += 1        
