63c8223b0c74eccca8fd1f6247b45311656977e3
EBolle <ernstbolle@gmail.com>
2024-01-04 23:04:18 +0100

    Simplfied the main function, and added better comments

--- src/writetight/main.py
+++ src/writetight/main.py
@@ -1,73 +1,47 @@
 import re
-import sys
-
-import spacy
-from spacy.matcher import Matcher
 
 from writetight.input_validation import (
     get_text_file,
     replace_markdown_style_operators,
 )
-from writetight.patterns import (
-    ambiguous_pronouns,
-    ambiguous_openings,
-    passive_voice,
-    ly_adverbs,
-)
 from writetight.pattern_questions import pattern_question
-
-
-try:
-    nlp = spacy.load("en_core_web_sm")
-except IOError as err:
-    print(f"You need the 'en_core_web_sm' language model from spaCy for write-tight to work.\n"
-          f"Please enter the following command in your terminal with your virtual environment activated: \n"
-          f"python -m spacy download en_core_web_sm\n"
-          f"Check the README for more info.")
-    sys.exit(1)
-
-
-matcher = Matcher(nlp.vocab)
-matcher.add("ambiguous-pronouns", [ambiguous_pronouns])
-matcher.add("ambiguous-openings", [ambiguous_openings])
-matcher.add("passive-voice", [passive_voice])
-matcher.add("ly-adverbs", [ly_adverbs])
+from writetight.spacy import get_language_model, get_matcher
 
 
 def main():
     """
-    This function reads a text file, which is processed in three ways.
-
-    1. As an nlp doc instance to get every pattern match and its location in the text file.
-    2. As a list of tokens to get the string match based on the match location.
-    3. As a list of line numbers and lines to link the location of the string match to the exact line number and column.
-
-    The matcher instance returns a list of matches with the start token of the match in ascending order.
-    This ascending order is leveraged to only try to match the first match found with the first line of the text file.
-    If there is a match, the second match is initialized. If there is no match, the second line of the text file is processed.
+    Better explanation here.
     """
+    # Get the raw text, and remove the markdown stlye operators.
     text = get_text_file()
     text_clean = re.sub(r"(_|\*)", replace_markdown_style_operators, text)
-    text_lines = text_clean.splitlines()
-    text_lines_num = [(line_num, line) for line_num, line in enumerate(text_lines)]
 
+    # Transform the raw text to a spaCy Doc to get the Token objects and matches.
+    nlp = get_language_model()
     doc = nlp(text)
     text_tokens = [token.text for token in doc]
+    matcher = get_matcher(nlp)
     matches = matcher(doc)
 
-    # matches is a list with matches in ascending order
+    # Split the raw text into lines to display the line number and column number of each match.
+    text_lines = text_clean.splitlines()
+    text_lines_numbered = [
+        (line_number, line) for line_number, line in enumerate(text_lines)
+    ]
+
+    # The 'matches' object contains a list of matches in ascending order of the text.
+    # Therefore the text can be analyzed efficiently line for line until all the matches
+    # are consumed. A new match starts from the beginning of the sentence of the last match
+    # to control for multiple matches in a single sentence.
     current_match = matches.pop(0)
     current_line_num = 0
 
     while matches:
-        pattern_id = current_match[0]
+        pattern_id, match_start_token, match_end_token = current_match
         pattern = nlp.vocab.strings[pattern_id]
-
-        match_start_token = current_match[1]
-        match_end_token = current_match[2]
         match = " ".join(text_tokens[match_start_token:match_end_token])
 
-        current_line = text_lines_num[current_line_num][1]
+        current_line = text_lines_numbered[current_line_num][1]
 
         match_position = re.search(rf"\b{match}\b", current_line)
         if match_position is not None:
