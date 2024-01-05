bff722096e5ec3b3b08df27b5e4c75bb70ff5a01
EBolle <ernstbolle@gmail.com>
2024-01-05 22:02:21 +0100

    Refactored the argument parser. Now all the logic of the input_validation module is written in functions

--- src/writetight/input_validation.py
+++ src/writetight/input_validation.py
@@ -14,19 +14,26 @@ def path_exists(path: str) -> str:
         raise FileNotFoundError(path)
 
 
-parser = argparse.ArgumentParser(
-    prog="write-tight",
-    description="this program validates whether your input text file exists.",
-)
-parser.add_argument("path", type=path_exists)
+def get_parsed_path():
+    """
+    This argument parser captures the input from stdin, and
+    validates the input text file path.
+    """
+    parser = argparse.ArgumentParser(
+        prog="write-tight",
+        description="This program validates whether your input text file exists.",
+    )
+    parser.add_argument("path", type=path_exists, help="path to your text file.")
+    args = parser.parse_args()
+
+    return args.path
 
 
 def get_text_file() -> str:
     """
     Return the text file if the path argument exists.
     """
-    args = parser.parse_args()
-    path: str = args.path
+    path: str = get_parsed_path()
 
     extension = path.rsplit(".")[-1]
     if extension.lower() != "md":
