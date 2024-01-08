00635568411e5cb9084cea63240ba6ccd821af27
EBolle <ernstbolle@gmail.com>
2024-01-08 22:01:04 +0100

    Added a test for the passive voice pattern and created a simple helper test function to avoid boilerplate code

--- tests/test_patterns.py
+++ tests/test_patterns.py
@@ -8,11 +8,23 @@ from writetight.patterns import (
 )
 
 
-def test_pattern(nlp, text, pattern, pattern_name):
+def get_match(nlp, text, pattern):
     """
-    Avoid boilerplate by using a simple function just for the tests.
+    Boilerplate code for getting the match. This function 
+    only returns the first match found.
     """
-    pass
+    doc = nlp(text)
+    text_tokens = [token.text for token in doc]
+
+    matcher = Matcher(nlp.vocab)
+    # the name of the pattern is not relevant for the tests
+    matcher.add("default", [pattern])
+    matches = matcher(doc)
+
+    _, match_start_token, match_end_token = matches[0]
+    match = " ".join(text_tokens[match_start_token:match_end_token])
+
+    return match
 
 
 def test_ambiguous_openings(nlp):
@@ -22,15 +34,7 @@ def test_ambiguous_openings(nlp):
     Or by specifying 'It': 'The wedding was amazing'.
     """
     text = "The weather was great and the food was lovely. It was amazing."
-    doc = nlp(text)
-    text_tokens = [token.text for token in doc]
-
-    matcher = Matcher(nlp.vocab)
-    matcher.add("ambiguous-openings", [ambiguous_openings])
-    matches = matcher(doc)
-
-    _, match_start_token, match_end_token = matches[0]
-    match = " ".join(text_tokens[match_start_token:match_end_token])
+    match = get_match(nlp, text, ambiguous_openings)
 
     assert match == "It was"
 
@@ -38,36 +42,31 @@ def test_ambiguous_openings(nlp):
 def test_ambiguous_pronouns(nlp):
     """
     What does 'this' refer to? The more specifc, the better.
-    'Please read the README.md file before you start.
+    'Please read the README.md file before you start.'
     """
     text = "Please read this before you start."
-    doc = nlp(text)
-    text_tokens = [token.text for token in doc]
-
-    matcher = Matcher(nlp.vocab)
-    matcher.add("ambiguous-pronouns", [ambiguous_pronouns])
-    matches = matcher(doc)
-
-    _, match_start_token, match_end_token = matches[0]
-    match = " ".join(text_tokens[match_start_token:match_end_token])
+    match = get_match(nlp, text, ambiguous_pronouns)
 
     assert match == "this"
 
 
 def test_ly_adverbs(nlp):
     """
-    Concise writing is better writing. Alternatives like great
-    or awesome are better.
+    Concise writing is better writing. Alternatives like 'great'
+    or 'awesome' are better.
     """
     text = "That movie is really good."
-    doc = nlp(text)
-    text_tokens = [token.text for token in doc]
-
-    matcher = Matcher(nlp.vocab)
-    matcher.add("ly-adverbs", [ly_adverbs])
-    matches = matcher(doc)
-
-    _, match_start_token, match_end_token = matches[0]
-    match = " ".join(text_tokens[match_start_token:match_end_token])
+    match = get_match(nlp, text, ly_adverbs)
 
     assert match == "really"
+
+
+def test_passive_voice(nlp):
+    """
+    Who are what modified the README.md file? 
+    'John Doe modified the README.md file.'
+    """
+    text = "The README.md file was modified with new content."
+    match = get_match(nlp, text, passive_voice)
+
+    assert match == "was modified"
